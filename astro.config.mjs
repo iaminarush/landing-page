b0aa6549253f6bfe25caf6fedab8e3e820fc68db
Patrick Cheng <patrickcheng1995@gmail.com>
2023-04-04 16:42:52 +0800

    upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://iaminarush.github.io",
   base: "/landing-page",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
