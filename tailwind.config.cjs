f88df51d379c0d2ebaec9f0e2cca13b84610bc6b
Patrick <patrickcheng1995@gmail.com>
2024-03-20 17:45:59 +0800

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     // fontSize: {
     // "6xl": [
@@ -17,5 +22,16 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
