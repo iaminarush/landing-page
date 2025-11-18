52d7697f214de2fc0617d634a2e2e2d54777ce11
Patrick <patrickcheng1995@gmail.com>
2025-11-18 17:51:29 +0800

    wip

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,8 +1,7 @@
 import dayjs from "dayjs";
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { FlickeringGrid } from "./ui/flickering-grid";
-import { TypingAnimation } from "./ui/typing-animation";
+import { HyperText } from "./ui/hyper-text";
 
 // export const Content = () => {
 //   return (
@@ -46,39 +45,6 @@ export const Content = () => {
 
     return () => window.removeEventListener("reisze", handleResize);
   }, []);
-
-  return (
-    <div className="bg-background relative h-dvh w-full overflow-hidden">
-      <FlickeringGrid
-        className="absolute inset-0 z-0 size-full"
-        squareSize={4}
-        gridGap={6}
-        color="#6B7280"
-        maxOpacity={0.5}
-        flickerChance={0.1}
-        height={dimensions.height + 10}
-        width={dimensions.width + 10}
-      />
-      <div className="container mx-auto">
-        <div className="relative flex flex-col gap-4 items-center justify-center px-4">
-          <span>
-            <TypingAnimation
-              className="text-3xl md:text-7xl font-space font-bold dark:text-white text-center tracking-tight"
-              cursorStyle="underscore"
-              typeSpeed={75}
-            >
-              Hello I am Patrick, a frontend developer building web and mobile
-              applications using React and React Native for
-            </TypingAnimation>
-            {/* <Experience /> */}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Experience = () => {
   const [experience, setExperience] = useState(
     dayjs()
       .diff(dayjs("2021-08-23", "YYYY-MM-DD"), "year", true)
@@ -94,15 +60,55 @@ const Experience = () => {
   }, []);
 
   return (
-    <TypingAnimation className="font-space text-rose-400">
-      {experience}
-    </TypingAnimation>
+    <div className="bg-background relative h-dvh w-full overflow-hidden">
+      <FlickeringGrid
+        className="absolute inset-0 z-0 size-full"
+        squareSize={4}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={dimensions.height + 10}
+        width={dimensions.width + 10}
+      />
+      <div className="container mx-auto">
+        <div className="relative flex flex-col gap-4 items-center justify-center px-4">
+          <div className="inline-flex items-baseline flex-wrap">
+            <HyperText animateOnHover={false} className="inline">
+              Hello I am Patrick, a frontend developer building web and mobile
+              applications using React and React Native for
+            </HyperText>
+            <Experience />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Experience = () => {
+  const [mounted, setMounted] = useState(false);
+  const [experience, setExperience] = useState(
+    dayjs()
+      .diff(dayjs("2021-08-23", "YYYY-MM-DD"), "year", true)
+      .toString()
+      .substring(0, 11)
   );
 
+  useEffect(() => {
+    setInterval(() => {
+      let time = dayjs().diff(dayjs("2021-08-23", "YYYY-MM-DD"), "year", true);
+      setExperience(time.toString().substring(0, 11));
+    }, 75);
+  }, []);
+  useEffect(() => setMounted(true), []);
+
   return (
-    <span className="font-space text-rose-400" id="experience">
-      {" "}
-      {experience}{" "}
-    </span>
+    <div
+      className="font-space text-rose-400"
+      key={mounted ? "animated" : "initial"}
+    >
+      {experience}
+    </div>
   );
 };
