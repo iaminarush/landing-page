ce9fcbd6a86bd737a4142c8e454d3bcff06a764f
Patrick <patrickcheng@tutanota.com>
2025-12-04 01:34:50 +0800

    remove unused code

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,30 +3,6 @@ import { useEffect, useState } from "react";
 import { FlickeringGrid } from "./ui/flickering-grid";
 import { HyperText } from "./ui/hyper-text";
 
-// export const Content = () => {
-//   return (
-//     <div className="container mx-auto">
-//       <motion.div
-//         initial={{ opacity: 0.0, y: 40 }}
-//         whileInView={{ opacity: 1, y: 0 }}
-//         transition={{
-//           delay: 0.3,
-//           duration: 0.8,
-//           ease: "easeInOut",
-//         }}
-//         className="relative flex flex-col gap-4 items-center justify-center px-4"
-//       >
-//         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-//           Hello I am Patrick, a frontend developer building web and mobile
-//           applications using React and React Native for
-//           <Experience />
-//           years.
-//         </div>
-//       </motion.div>
-//     </div>
-//   );
-// };
-
 export const Content = () => {
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
