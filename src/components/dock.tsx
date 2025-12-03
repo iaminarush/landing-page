323843a03702b655ad5574a3ecb77850e8127db6
Patrick <patrickcheng@tutanota.com>
2025-12-04 01:34:54 +0800

    use new dock

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -1,32 +1,12 @@
-import type { ReactNode } from "react";
-import { FloatingDock } from "./aceternity/floating-dock";
 import {
   IconBrandGithub,
   IconBrandLinkedin,
   IconMail,
 } from "@tabler/icons-react";
 import { motion } from "framer-motion";
+import { Dock, DockIcon } from "./ui/dock.tsx";
 
-const FLOATING_DOCK_ITEMS: { title: string; icon: ReactNode; href: string }[] =
-  [
-    {
-      title: "Github",
-      icon: <IconBrandGithub className="h-full w-full text-neutral-300" />,
-      href: "https://github.com/iaminarush/",
-    },
-    {
-      title: "Email",
-      icon: <IconMail className="h-full w-full text-neutral-300" />,
-      href: "mailto:patrickcheng@tutanota.com",
-    },
-    {
-      title: "Linkedin",
-      icon: <IconBrandLinkedin className="h-full w-full text-neutral-300" />,
-      href: "https://www.linkedin.com/in/patrick-cheng-020643166/",
-    },
-  ];
-
-export const Dock = () => {
+export const DockContainer = () => {
   return (
     <motion.div
       initial={{ opacity: 0.0, y: 40 }}
@@ -38,7 +18,28 @@ export const Dock = () => {
       }}
       className="fixed bottom-8 flex items-center justify-center w-full h-[12rem]"
     >
-      <FloatingDock items={FLOATING_DOCK_ITEMS} />
+      <Dock>
+        <DockIcon>
+          <a href="https://github.com/iaminarush/" target="_blank">
+            <IconBrandGithub className="h-full w-full text-neutral-300" />
+          </a>
+        </DockIcon>
+
+        <DockIcon>
+          <a href="mailto:patrickcheng@tutanota.com">
+            <IconMail className="h-full w-full text-neutral-300" />
+          </a>
+        </DockIcon>
+
+        <DockIcon>
+          <a
+            href="https://www.linkedin.com/in/patrick-cheng-020643166/"
+            target="_blank"
+          >
+            <IconBrandLinkedin className="h-full w-full text-neutral-300" />
+          </a>
+        </DockIcon>
+      </Dock>
     </motion.div>
   );
 };
